84d38ac996cb04c485b40a978c3a46bfd475e32f
goktugocal <goktugocal41@gmail.com>
2023-11-05 21:27:19 +0300

    new train

--- scripts/fed/train_cpu_server_model.sh
+++ scripts/fed/train_cpu_server_model.sh
@@ -1,5 +1,5 @@
 /home/goktug.ocal/py_envs/netadapt/bin/python train_server_model.py \
     data/ \
-    --dir models/alexnet/model_cpu_2.pth.tar \
+    --dir models/alexnet/model_cpu_3.pth.tar \
     --arch alexnet \
     # --no-cuda
