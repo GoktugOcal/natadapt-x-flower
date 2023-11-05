bc45a4e948f7fd9d435397e3613337660393729d
goktugocal <goktugocal41@gmail.com>
2023-11-05 21:04:27 +0300

    new test cpu 2

---
+++ scripts/test/test_8.sh
@@ -0,0 +1,8 @@
+python test_master.py \
+    models/alexnet/test/test-8 \
+    3 224 224 \
+    -im models/alexnet/model_cpu_2.pth.tar -gp 0 \
+    -mi 30 -bur 0.25 -rt FLOPS -irr 0.05 -rd 0.96 \
+    -lr 0.001 -st 500 \
+    -dp data/Cifar10/server --arch alexnet \
+    -nc 0
