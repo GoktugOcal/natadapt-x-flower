35c833401f6a426fe496840a15ec315da52f47fe
goktugocal <goktugocal41@gmail.com>
2023-11-29 00:20:44 +0300

    03

--- scripts/fed/cifar_niid.sh
+++ scripts/fed/cifar_niid.sh
@@ -1,5 +1,5 @@
 python generate_cifar10.py \
-    -dir ./data/32_Cifar10_NIID_20c_a01/ \
+    -dir ./data/32_Cifar10_NIID_20c_a03/ \
     -nc 140 \
     -c 10 \
     -niid noniid \
---
+++ scripts/fed/train_32_server_NIID_140c_a03.sh
@@ -0,0 +1,4 @@
+/home/goktug.ocal/py_envs/netadapt/bin/python train_server_model.py \
+    data/32_Cifar10_NIID_20c_a03/ \
+    --dir models/alexnet/alexnet32_a03_server.pth.tar \
+    --arch alexnet_reduced
