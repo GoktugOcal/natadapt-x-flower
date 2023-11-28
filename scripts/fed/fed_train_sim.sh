f9585e99ea5aa016df9ba837182158b6b6b67912
goktugocal <goktugocal41@gmail.com>
2023-11-28 15:21:15 +0300

    kill

--- scripts/fed/fed_train_sim.sh
+++ scripts/fed/fed_train_sim.sh
@@ -4,8 +4,8 @@ for alpha in 0.3 0.5; do
         -pf ./projects/define_pretrained_fed_sim_NIID_alpha${str_alpha}/ \
         -m alexnet.pth.tar \
         -nc 140 \
-        -nr 50 \
-        --fine_tuning_epochs 10 \
+        -nr 200 \
+        --fine_tuning_epochs 50 \
         -niid noniid \
         -b - \
         -p dir \
