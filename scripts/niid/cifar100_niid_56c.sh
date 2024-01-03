4ec7cb859669da78552604dbc8ed8eb42ea895b1
goktugocal <goktugocal41@gmail.com>
2024-01-04 00:08:45 +0300

    01

--- scripts/niid/cifar100_niid_56c.sh
+++ scripts/niid/cifar100_niid_56c.sh
@@ -1,8 +1,8 @@
 python generate_cifar100.py \
-    -dir ./data/32_Cifar100_NIID_56c_a03/ \
+    -dir ./data/32_Cifar100_NIID_56c_a01/ \
     -nc 56 \
     -c 100 \
     -niid noniid \
     -b - \
     -p dir \
-    -a 0.3
+    -a 0.1
