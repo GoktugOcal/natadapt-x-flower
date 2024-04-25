4b2a6308f956dc4c1b885f8c29975a24688a451c
goktugocal <goktugocal41@gmail.com>
2024-04-26 01:44:04 +0200

    i

--- pure_core.py
+++ pure_core.py
@@ -176,21 +176,11 @@ def main(args):
     block_id = [block_id for block_id in ["block_0","block_1","block_2","block_3","block_4","block_5","block_6"] if block_id in MODEL_PATH][0]    
     # client_networks = client_networks_all[block_id]
 
-    client_networks_all = {
-        0: None,
-        1: None,
-        2: None,
-        3: None,
-        4: None,
-        5: None,
-        6: None,
-    }
+    client_networks_all = {}
     with open(os.path.join("./data",DATASET_PATH,"client_groups.json")) as json_file:
         bw_data = json.load(json_file)
     for cid, tier in bw_data[block_id].items():
         print(cid, tier)
-        print(tiers)
-        print(client_networks_all[int(cid)])
         client_networks_all[cid] = tiers[tier]
     client_networks = client_networks_all
 
