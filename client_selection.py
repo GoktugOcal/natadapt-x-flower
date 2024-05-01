f717c7d77435fb605140ebc5d69403e585dea791
goktugocal <goktugocal41@gmail.com>
2024-05-01 15:35:32 +0200

    starter 1

--- client_selection.py
+++ client_selection.py
@@ -264,7 +264,7 @@ class ClientSelector:
         self.groupdf["group"] = groups
     
     def network_optimized_grouping(self, model_metadata):
-        grp_no = 0
+        grp_no = 1
         replace_dict = {}
         for k, v in model_metadata.items():
             replace_dict[grp_no] = k
