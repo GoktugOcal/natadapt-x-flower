255bc7d651c87ba3922d92a95f3fced583ec889c
goktugocal <goktugocal41@gmail.com>
2024-05-07 23:01:29 +0200

    scripts

---
+++ slurm/fed_master_basic_a03_210c_fedprox_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
+#SBATCH --container-mount-home
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+#SBATCH --time=60:00:00
+#SBATCH -o fed_master_basic_a03_210c_fedprox.log
+
+source /opt/python3/venv/base/bin/activate
+#git clone https://github.com/GoktugOcal/netadapt-x-flower.git
+cd /users/goktug.ocal/thesis/netadapt-x-flower
+pip install -r requirements.txt
+pip install torch==2.0.1 torchvision==0.15.2
+nvidia-smi
+sh scripts/fed_master/fed_prox/fed_master_basic_a03_210c_fedprox.sh
