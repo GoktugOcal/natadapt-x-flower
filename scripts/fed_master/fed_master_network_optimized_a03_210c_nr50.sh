b4fe9624f163d4536a92f8ea1a4e90fba7d06e96
goktugocal <goktugocal41@gmail.com>
2024-05-09 00:59:49 +0200

    112

---
+++ scripts/fed_master/fed_master_network_optimized_a03_210c_nr50.sh
@@ -0,0 +1,14 @@
+python test_fed_master.py \
+    projects/fed_master/fed_master_network_optimized_a03_210c_nr50\
+    3 32 32 \
+    -im ./projects/pretrained_model/predefined_a03_210c_fedavg/last_model.pth.tar \
+    -c 10 \
+    -gp 0 \
+    -mi 15 -bur 0.25 -rt FLOPS -irr 0.025 -rd 0.96 \
+    -lr 0.001 -st 500 \
+    -dp ./data/alpha/Cifar10_NIID_210c_a03 \
+    --arch alexnet \
+    -nc 210 \
+    -nr 50 \
+    --fine_tuning_epochs 50 \
+    --client_selection "network"
---
+++ slurm/112/fed_master_optimized_a03_112c_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
+#SBATCH --container-mount-home
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+#SBATCH --time=60:00:00
+#SBATCH -o slurm_fed_master_optimized_a03_112c_1.log
+
+source /opt/python3/venv/base/bin/activate
+#git clone https://github.com/GoktugOcal/netadapt-x-flower.git
+cd /users/goktug.ocal/thesis/netadapt-x-flower
+pip install -r requirements.txt
+pip install torch==2.0.1 torchvision==0.15.2
+nvidia-smi
+sh scripts/fed_master/fed_master_optimized_a03_112c_1.sh
