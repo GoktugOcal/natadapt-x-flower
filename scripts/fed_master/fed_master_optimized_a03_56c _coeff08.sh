2ae11f422db776aaa591703eb85c40ac7e22b9e0
goktugocal <goktugocal41@gmail.com>
2024-07-11 00:01:30 +0200

    coeff08

---
+++ scripts/fed_master/fed_master_optimized_a03_56c _coeff08.sh
@@ -0,0 +1,14 @@
+python test_fed_master.py \
+    projects/fed_master/fed_master_optimized_2_a03_56c_coeff08\
+    3 32 32 \
+    -im ./projects/pretrained_model/predefined_a03_56c_fedavg/last_model.pth.tar \
+    -c 10 \
+    -gp 0 \
+    -mi 15 -bur 0.25 -rt FLOPS -irr 0.025 -rd 0.96 \
+    -lr 0.001 -st 500 \
+    -dp ./data/alpha/Cifar10_NIID_56c_a03 \
+    --arch alexnet \
+    -nc 56 \
+    -nr 20 \
+    --fine_tuning_epochs 50 \
+    --client_selection "optimized"
